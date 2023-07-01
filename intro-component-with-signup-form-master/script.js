d901c8cf66777323deb1b36757244fef1a945256
Rob Simpson <rsimpson2@me.com>
2023-07-01 15:59:48 -0400

    updates from code review

--- intro-component-with-signup-form-master/script.js
+++ intro-component-with-signup-form-master/script.js
@@ -1,16 +1,29 @@
 const formInputs = Array.from(document.querySelectorAll('input'));
 const submitBtn = document.querySelector('.submit-btn');
 
+function toggleErrorMessages(input) {
+  if (input.validity.valueMissing) {
+    input.classList.add('invalid');
+    document.querySelector(`.error-${input.name}`).classList.remove('display-none');
+    document.querySelector(`.error-icon-${input.name}`).classList.remove('display-none');
+  } else {
+    input.classList.remove('invalid');
+    document.querySelector(`.error-${input.name}`).classList.add('display-none');
+    document.querySelector(`.error-icon-${input.name}`).classList.add('display-none');
+  }
+}
+
 window.addEventListener('DOMContentLoaded', () => {
+  formInputs.forEach(input => {
+    input.addEventListener('blur', () => {
+      toggleErrorMessages(input);
+    });
+  });
+
   submitBtn.addEventListener('click', (e) => {
     e.preventDefault();
     formInputs.forEach(input => {
-      console.log(input.name);
-      if (input.validity.valueMissing) {
-        input.classList.add('invalid');
-        document.querySelector(`.error-${input.name}`).classList.remove('display-none');
-        document.querySelector(`.error-icon-${input.name}`).classList.remove('display-none');
-      }
+      toggleErrorMessages(input);
     });
   });
 });

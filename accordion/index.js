6ecef7355959e3e2a0e35f48ecb57aa8c89db66b
Rob Simpson <rsimpson2@me.com>
2025-05-07 20:21:51 -0400

    create execution context

--- accordion/index.js
+++ accordion/index.js
@@ -1,22 +1,24 @@
-// Get all details elements
-const detailsElements = document.querySelectorAll("details");
+(() => {
+  // Get all details elements
+  const detailsElements = document.querySelectorAll("details");
 
-// Apply the initial state for any open details
-detailsElements.forEach((details) => {
-  const icon = details.querySelector(".accordion-icon");
-  if (details.open) {
-    icon.classList.add("accordion-icon--rotated");
-  }
-});
-
-// Add event listeners to all details elements
-detailsElements.forEach((details) => {
-  details.addEventListener("toggle", function () {
-    const icon = this.querySelector(".accordion-icon");
-    if (this.open) {
+  // Apply the initial state for any open details
+  detailsElements.forEach((details) => {
+    const icon = details.querySelector(".accordion-icon");
+    if (details.open) {
       icon.classList.add("accordion-icon--rotated");
-    } else {
-      icon.classList.remove("accordion-icon--rotated");
     }
   });
-});
+
+  // Add event listeners to all details elements
+  detailsElements.forEach((details) => {
+    details.addEventListener("toggle", function () {
+      const icon = this.querySelector(".accordion-icon");
+      if (this.open) {
+        icon.classList.add("accordion-icon--rotated");
+      } else {
+        icon.classList.remove("accordion-icon--rotated");
+      }
+    });
+  });
+})();
